539531187a857918b6648570c755ce212f5391b1
freds.hc <fahc95@gmail.com>
2020-08-27 14:53:01 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+} from "@react-google-maps/api";
+import SearchBox from "./components/SearchBox/SearchBox.jsx"
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const coordsBarcelona = {
+  lat: 41.384747,
+  lng: 2.176391
+};
+
+const libraries = ["places"];
+
+const mapContainerStyle = {
+  width: '70vw',
+  height: '50vh'
+};
+
+const mapOptions = {
+  disableDefaultUI: true,
+  zoomControl: true
+}
+
+export default function App() {
+  // const [logged, setLoginStatus] = useState(false);
+  // const [photo, setPhoto] = useState('');
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return "Error loading Maps";
+  if (!isLoaded) return "Loading...";
+  if (isLoaded) console.log("API Loaded");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="my-5 d-flex justify-content-center">
+        <SearchBox lat={coordsBarcelona.lat} lng={coordsBarcelona.lng} />
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={12}
+          center={coordsBarcelona}
+          options={mapOptions}
+        ></GoogleMap>
+      </div>
     </div>
   );
 }
 
-export default App;
